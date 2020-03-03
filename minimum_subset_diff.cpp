53f5e703769bab7a7feca6a26a574a0ab089b8d9
scheninsp <songchen7788@163.com>
2020-03-03 22:36:47 +0800

    0303
    0303

---
+++ minimum_subset_diff.cpp
@@ -0,0 +1,71 @@
+//partition a set into two subsets with minimum difference
+//require output the difference
+
+//exp.
+//input: 1, 6, 11, 5
+//output: 1
+//Subset1 = { 1, 5, 6 }
+//Subset2 = { 11 }
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+short readFile(vector<vector<int>> &elements) {
+
+	short errorcode = 0;
+
+	ifstream frd("./input.txt");   //format: sum, elem1, elem2 ... elemN
+	const int maxLine = 256;
+	char buf[maxLine] = { 0 };
+	char* tmp;
+
+	while (frd.getline(buf, maxLine)) {
+		vector<int> one_row;
+		tmp = strtok(buf, " ");
+		while (tmp) {
+			int num = atoi(tmp);
+			tmp = strtok(NULL, " ");
+			one_row.push_back(num);
+		}
+		elements.push_back(one_row);
+	}
+
+	return errorcode;
+
+};
+
+
+int findMinRec(int arr[], int i, int sc, int st)
+{
+	if (i == 0)
+		return abs((st - sc) - sc);
+
+	return min(findMinRec(arr, i - 1, sc + arr[i - 1], st),
+		findMinRec(arr, i - 1, sc, st));
+}
+
+int findMin(int arr[], int n)
+{
+	int st = 0;
+	for (int i = 0; i < n; i++)
+		st += arr[i];
+
+	return findMinRec(arr, n, 0, st);
+}
+
+// Driver program to test above function 
+int main()
+{
+	int arr[] = { 1, 6, 11, 5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "diff : " << findMin(arr, n) << endl;
+	
+	getchar();
+	return 0;
+}
