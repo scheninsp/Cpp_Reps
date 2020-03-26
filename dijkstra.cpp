e3585e2e7d8e66a02b2aa54fd8d2cb8bdefda8a9
scheninsp <songchen7788@163.com>
2020-03-27 00:38:12 +0800

    graph
    graph

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -64,19 +64,68 @@ public:
 		}
 	}
 
+	void dijkstra(int* dist, int v) {
+
+		bool* visited = new  bool[nv];
+		for (int i = 0; i < nv; i++) {
+			visited[i] = false;
+		}
+
+		dist[v] = 0;
+
+		for (int i = 0; i < nv - 1; i++) {  //visit nv-1 vertex will finish 
+
+			//get a unprocessed vertex with current shortest path to v
+			int shortest = INT_MAX;
+			int  u = 0;
+			for (int i = 0; i < nv; i++) {
+				if (visited[i] == false && dist[i] < shortest) {
+					u = i;
+					shortest = dist[i];
+				}
+			}
+			visited[u] = true;
+
+			//update dist
+			for (int j = 0; j < nv; j++) {
+				//dist[u]!=INT_MAX to avoid cross limit
+				if (!visited[j] && mat[u][j] && dist[u] != INT_MAX && dist[u] + mat[u][j] < dist[j]) {
+					dist[j] = dist[u] + mat[u][j];
+				}
+			}
+
+		}
+
+		delete visited;
+	}
+
 };
 
 
 
 int main() {
 
+	int n = 9;
 
-	Graph graph(9);
+	Graph graph(n);
 
 	graph.setMatFromFile("input.txt");
 	graph.printMat();
 
-	//dijkstra(graph, 0);
+	int* dist = new int[n];
+	for (int i = 0; i < n; i++) {
+		dist[i] = INT_MAX;
+	}
+
+	int vst = 0;
+
+	graph.dijkstra(dist, vst);
+
+	std::cout << "dist from " << vst << std::endl ;
+	for (int i = 0; i < n; i++) {
+		std::cout << dist[i] << " ";
+	}
+	std::cout << std::endl;
 
 	getchar();
 	return 0;
