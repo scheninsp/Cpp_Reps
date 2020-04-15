5e87e5cc6f3c414885e36f3639bc5c445d46baac
scheninsp <songchen7788@163.com>
2020-04-16 00:05:26 +0800

    string
    string

--- binary_palindrome .cpp
+++ binary_palindrome .cpp
@@ -1,34 +1,43 @@
 //binary palindrome 
+//palindrome must start with 1 and has maximum number of 0
+//exp.
+//input:
+//n=5, k=3
+//output: 11011
+//n=10, k=4
+//output: 1100110011
+//n=2, k=8
+//output: 11
 
 #include <iostream> 
 #include <vector> 
 using namespace std; 
   
-void dfs(int parent, int ans[], vector<int> connectchars[]) 
+void dfs(int parent, int ans[], vector<int> str[]) 
 { 
     ans[parent] = 1; 
   
-    for (int i = 0; i < connectchars[parent].size(); i++) { 
-        if (!ans[connectchars[parent][i]]) 
-            dfs(connectchars[parent][i], ans, connectchars); 
+    for (int i = 0; i < str[parent].size(); i++) { 
+        if (!ans[str[parent][i]]) 
+            dfs(str[parent][i], ans, str); 
     } 
 } 
   
-void printBinaryPalindrome(int n, int k) 
+void binary_palindrome(int n, int k) 
 { 
     int arr[n], ans[n] = { 0 }; 
   
-    vector<int> connectchars[k]; 
+    vector<int> str[k]; 
   
     for (int i = 0; i < n; i++) 
         arr[i] = i % k; 
   
   for (int i = 0; i < n / 2; i++) { 
-        connectchars[arr[i]].push_back(arr[n - i - 1]); 
-        connectchars[arr[n - i - 1]].push_back(arr[i]); 
+        str[arr[i]].push_back(arr[n - i - 1]); 
+        str[arr[n - i - 1]].push_back(arr[i]); 
     } 
   
-    dfs(0, ans, connectchars); 
+    dfs(0, ans, str); 
   
     for (int i = 0; i < n; i++) 
         cout << ans[arr[i]]; 
@@ -37,6 +46,6 @@ void printBinaryPalindrome(int n, int k)
 int main() 
 { 
     int n = 10, k = 4; 
-    printBinaryPalindrome(n, k); 
+    binary_palindrome(n, k); 
     return 0; 
 } 
